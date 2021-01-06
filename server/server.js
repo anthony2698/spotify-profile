13e4ed4dc20c902a3e8a3c64e3ec5ca908c61a8c
Anthony Carrillo <carrilloanthony2698@gmail.com>
2021-01-06 17:41:35 -0500

    Fixed Login and Callback endpoint to use axios.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 //packages needed for spotify authorization
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const querystring = require('query-string');
@@ -14,8 +15,8 @@ let frontend_uri = process.env.FRONTEND_URI || 'http://localhost:3000';
 const port = process.env.PORT || 8000;
 
 if (process.env.NODE_ENV !== 'production') {
-    REDIRECT_URI = 'http://localhost:8888/callback';
-    FRONTEND_URI = 'http://localhost:3000';
+    redirect_uri = 'http://localhost:8000/callback';
+    frontend_uri = 'http://localhost:3000';
 }
 
 //server packages applications
@@ -43,25 +44,25 @@ const stateKey = 'spotify_auth_state';
 //login endpoint
 server.get('/login', (req, res) => {
     //state
-    var state = generateRandomString(16);
+    const state = generateRandomString(16);
 
     //sent cookie with stateKey and state that was generated
     res.cookie(stateKey, state);
 
     //scope of the options we will access to through the app
-    var scope = 'user-read-private user-read-email user-read-recently-played user-top-read user-follow-read user-follow-modify playlist-read-private playlist-read-collaborative playlist-modify-public';
+    const scope = 'user-read-private user-read-email user-read-recently-played user-top-read user-follow-read user-follow-modify playlist-read-private playlist-read-collaborative playlist-modify-public';
 
     //redirects to spotify authorization tool, with needed params
     res.redirect(`https://accounts.spotify.com/authorize?${querystring.stringify({
-        response_type: 'code',
         client_id: client_id,
-        scope: scope,
+        response_type: 'code',
         redirect_uri: redirect_uri,
-        state: state
+        scope: scope,
+        state: state,
     })}`);
 });
 
-//callback endpoint
+// callback endpoint
 server.get('/callback', (req, res) => {
     //the application request refresh and access tokens, after checking state parameter
     const code = req.query.code || null;
@@ -73,70 +74,65 @@ server.get('/callback', (req, res) => {
     if(state == null || state !== storedState) {
         res.redirect(`/#${querystring.stringify({ error: 'state mismatch' })}`)
     } else {
-        res.clearCookie(stateKey)
-        const authOptions = {
+        res.clearCookie(stateKey);
+        axios({
+            method: 'post',
             url: 'https://accounts.spotify.com/api/token',
-            form: {
-                code: code,
-                redirect_uri: redirect_uri,
-                grant_type: 'authorization_code',
-            },
             headers: {
-                Authorization: `Basic ${new Buffer.from(`${client_id}:${client_secret}`).toString(
-                    'base64',
-                )}`,
+                'Authorization': `Basic ${new Buffer.from(`${client_id}:${client_secret}`).toString('base64')}`,        
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            params: {
+              grant_type: 'authorization_code',
+              code: code,
+              redirect_uri: redirect_uri,
             },
             json: true,
-        };
-        axios.post(authOptions, (error, response, body) => {
-            if(!error && response.stausCode === 200) {
-                const access_token = body.access_token;
-                const refresh_token = body.refresh_token;
-
-                //we can pass the tokens to the browser to make request from there
+            })
+            .then(body => {
+                const access_token = body.data.access_token;
+                const refresh_token = body.data.refresh_token;
+                console.log(body)
                 res.redirect(
                     `${frontend_uri}/#${querystring.stringify({
-                        access_token,
-                        refresh_token
+                      access_token,
+                      refresh_token,
                     })}`,
-                );
-            } else {
-                res.redirect(
-                    `/#${querystring.stringify({ error: 'invalid token' })}`
-                )
-            }
-        });
+                  );
+            })
+            .catch(error => {
+                console.log(error)
+                res.redirect(`/#${querystring.stringify({ error: 'invalid_token' })}`);
+            });
     }
 });
 
-//refresh token endpoint, to request access token from refresh token. When we post we check to see if we get no errors and the code is 200, if so we send the new access token
-server.get('/refresh_token', (req, res) => {
-    const refresh_token = req.query.refresh_token;
-    const authOptions = {
-        url: 'https://accounts.spotify.com/api/token',
-        headers: {
-            Authorization: `Basic ${new Buffer.from(`${client_id}:${client_secret}`).toString(
-                'base64',
-            )}`,
-        },
-        form: {
-            grant_type: 'refresh_token',
-            refresh_token,
-        },
-        json: true,
-    };
-    axios.post(authOptions, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-            const access_token = body.access_token;
-            res.send({ access_token });
-        }
-    });
-});
+// //refresh token endpoint, to request access token from refresh token. When we post we check to see if we get no errors and the code is 200, if so we send the new access token
+// server.get('/refresh_token', (req, res) => {
+//     const refresh_token = req.query.refresh_token;
+//     const authOptions = {
+//         url: 'https://accounts.spotify.com/api/token',
+//         headers: {
+//             Authorization: `Basic ${new Buffer.from(`${client_id}:${client_secret}`).toString(
+//                 'base64',
+//             )}`,
+//         },
+//         form: {
+//             grant_type: 'refresh_token',
+//             refresh_token,
+//         },
+//         json: true,
+//     };
+//     axios.post(authOptions.url, authOptions)
+//         .then(res => { 
+//             res.send({ access_token });
+//         })
+// });
 
 // All remaining requests return the React app, so it can handle routing.
-server.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/public', 'index.html'));
-});
+// server.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client/public', 'index.html'));
+// });
 
 module.exports = server;
 
