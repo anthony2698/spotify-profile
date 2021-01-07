574d1dfe6bc93c4a5c824410a393b3661e610413
Anthony Carrillo <carrilloanthony2698@gmail.com>
2021-01-07 17:50:19 -0500

    Fixed token bug.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
-import { useState } from 'react';
-import './App.css';
+import { useState, useEffect } from 'react';
+
+import { token as accessToken } from './spotify';
+
+import { AppContainer } from './styles/AppStyles.js';
+
+import Profile from './components/Profile.js';
+import LoginScreen from './components/LoginScreen.js';
 
 function App() {
-  const [token, setToken] = useState()
-  return (
-    <div>
-      
-    </div>
-  );
+    const [token, setToken] = useState({ token: '' });
+
+    useEffect(() => {
+        setToken({ token: accessToken })
+    }, [])
+    
+    return (
+        <AppContainer>
+            {token.token ? <Profile /> : <LoginScreen />}
+        </AppContainer>
+    );
 }
 
 export default App;
