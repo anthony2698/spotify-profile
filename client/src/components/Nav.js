404960d437644162643c7ed3a83d7a558ef07eb4
Anthony Carrillo <carrilloanthony2698@gmail.com>
2021-01-11 16:22:08 -0500

    barebones Nav completed.

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Nav = () => {
     return (
-        <div>Nav</div>
+        <nav>
+            <div>
+                <Link to='/'>
+                    Spotify Icon
+                </Link>
+            </div>
+            <ul>
+                <li>
+                    <NavLink to='/'>
+                        <div>Profile</div>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/artists'>
+                        <div>Top Artists</div>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/tracks'>
+                        <div>Top Tracks</div>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/recentlyPlayed'>
+                        <div>Recently Played</div>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/playlists'>
+                        <div>Playlists</div>
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+
     )
 }
 
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -2,11 +2,29 @@ import React from 'react';
 
 import ScrollToTop from './ScrollToTop.js';
 import Nav from './Nav.js';
+import RecentlyPlayed from './RecentlyPlayed.js';
+import TopArtists from './TopArtists.js';
+import Playlists from './Playlists.js';
+import Playlist from './Playlist.js';
+import Artist from './Artist.js';
+import User from './User.js';
+import Track from './Track.js';
+import TopTracks from './TopTracks.js';
 
 const Profile = () => {
     return(
         <div>
             <Nav />
+            <ScrollToTop path='/'>
+                <User path='/'/>
+                <RecentlyPlayed path='recentlyPlayed'/>
+                <TopArtists path='artists' />
+                <TopTracks path='tracks' />
+                <Playlists path='playlists' />
+                <Playlist path='playlist/:playlistID' />
+                <Track path='track/:trackID' />
+                <Artist path='artist/:artistID' />
+            </ScrollToTop>
         </div>
     )
 }
