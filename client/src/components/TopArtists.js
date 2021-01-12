559b004baaa77db6d9ee21c37298a6f4349fd0b9
Anthony Carrillo <carrilloanthony2698@gmail.com>
2021-01-12 10:47:56 -0500

    Working on TopArtists component

--- client/src/components/TopArtists.js
+++ client/src/components/TopArtists.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { getTopArtistsShort, getTopArtistsMedium, getTopArtistsLong } from '../spotify';
+import { catchErrors } from '../utils';
+
+import Loader from './Loader.js';
 
 const TopArtists = () => {
+    const [data, setData] = useState({
+        topArtists: null,
+        activeRange: 'long',
+    });
+
+    apiCalls = {
+        long: getTopArtistsLong(),
+        medium: getTopArtistsMedium(),
+        short: getTopArtistsShort(),
+    }
+
+    useEffect(() => {
+        catchErrors(getData());
+    }, []);
+
+    async function getData() {
+        const { data } = await getTopArtistsLong();
+        setData({ topArtists: data });
+    }
+
+    async function changeRange(range) {
+        const { data } = await apiCalls[range];
+        setData({ topArtists: data, activeRange: range });
+    }
+
+    const setActiveRange = range => catchErrors(changeRange(range));
+
     return (
-        <div>TopArtists</div>
+        <main>
+            <header>
+                <h2>Top Artists</h2>
+                <div>
+                
+                </div>
+            </header>
+        </main>
     )
 }
 
