c27e2ddc3791f055b9e3ecf384152e0a4bca1de0
Anthony Carrillo <carrilloanthony2698@gmail.com>
2021-01-17 14:39:26 -0500

    Finished Track component.

--- client/src/components/Track.js
+++ client/src/components/Track.js
@@ -18,7 +18,7 @@ const Track = () => {
 
     useEffect(() => {
         catchErrors(getData());
-    });
+    }, []);
 
     async function getData() {
         const { track, audioAnalysis, audioFeatures } = await getTrackInfo(trackId);
@@ -39,8 +39,82 @@ const Track = () => {
                         <div>
                             <img src={track.album.images[0].url} alt='Album Artwork' />
                         </div>
+                        <div>
+                            <h1>{track.name}</h1>
+                            <h2>
+                                {track.artists && track.artists.map(({ name }, i) => (
+                                    <span key={i}>
+                                        {name}
+                                        {track.artists.length > 0 && i === track.artists.length - 1 ? '' : ','}
+                                        &nbsp;
+                                    </span>
+                                ))}
+                            </h2>
+                            <h3>
+                                <a
+                                    href={track.album.external_urls.spotify}>
+                                        {track.album.name}
+                                </a>{' '}
+                                &middot; {getYear(track.album.release_date)}
+                            </h3>
+                            <a
+                                href={track.album.external_urls.spotify}>
+                                    Play on Spotify
+                            </a>
+                        </div>
                     </div>
+
+                    {audioFeatures && audioAnalysis && (
+                        <div>
+                            <div>
+                                <div>
+                                    <h4>{formatDuration(audioFeatures.duration_ms)}</h4>
+                                    <p>Duration</p>
+                                </div>
+                                <div>
+                                    <h4>{parsePitchClass(audioFeatures.key)}</h4>
+                                    <p>Key</p>
+                                </div>
+                                <div>
+                                    <h4>{audioFeatures.mode === 1 ? 'Major' : 'Minor'}</h4>
+                                    <p>Modality</p>
+                                </div>
+                                <div>
+                                    <h4>{audioFeatures.time_signature}</h4>
+                                    <p>Time Signature</p>
+                                </div>
+                                <div>
+                                    <h4>{Math.round(audioFeatures.tempo)}</h4>
+                                    <p>Tempo (BPM)</p>
+                                </div>
+                                <div>
+                                    <h4>{track.popularity}%</h4>
+                                    <p>Popularity</p>
+                                </div>
+                                <div>
+                                    <h4>{audioAnalysis.beats.length}</h4>
+                                    <p>Bars</p>
+                                </div>
+                                <div>
+                                    <h4>{audioAnalysis.beats.length}</h4>
+                                    <p>Beats</p>
+                                </div>
+                                <div>
+                                    <h4>{audioAnalysis.sections.length}</h4>
+                                    <p>Sections</p>
+                                </div>
+                                <div>
+                                    <h4>{audioAnalysis.segments.length}</h4>
+                                    <p>Segments</p>
+                                </div>
+                            </div>
+
+                            <FeatureChart features={audioFeatures} type=""/>
+                        </div>
+                    )}
                 </main>
+            ) : (
+                <Loader />
             )}
         </>
     )
