8a145f1f023bc5c72d18dc51d756835f313eb4f7
Anthony Carrillo <carrilloanthony2698@gmail.com>
2021-01-18 17:45:26 -0500

    Working on heroku bug.

--- server/server.js
+++ server/server.js
@@ -41,6 +41,10 @@ const generateRandomString = (length) => {
 //key name we will pass into the cookie with actual state
 const stateKey = 'spotify_auth_state';
 
+server.get('/', function (req, res) {
+    res.render(path.resolve(__dirname, '../client/build/index.html'));
+  });
+
 //login endpoint
 server.get('/login', (req, res) => {
     //state
