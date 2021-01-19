cbe283ab7f245791f2d96a8fc806d1e4dfeb99de
Anthony Carrillo <carrilloanthony2698@gmail.com>
2021-01-19 09:09:55 -0500

    still working on it.

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ const generateRandomString = (length) => {
 const stateKey = 'spotify_auth_state';
 
 server.get('/', function (req, res) {
-    res.render('../client/build/index.html')
+    res.render(path.resolve(__dirname, '../client/build/index.html'));
 });
 
 //login endpoint
